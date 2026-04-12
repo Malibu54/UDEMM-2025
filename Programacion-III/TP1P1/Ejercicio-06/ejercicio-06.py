5c3e509383f26fd44e9d97c6d2c4e00d30714684
~Oriana~ <deciimesool@icloud.com>
2026-04-12 20:19:59 -0300

    Update ejercicio-06.py

---
+++ Programacion-III/TP1P1/Ejercicio-06/ejercicio-06.py
@@ -0,0 +1,19 @@
+# ── ¿Cómo lo haría teniendo en cuenta que solo puede existir una sola instancia?
+"""
+
+"""
+
+# ── ¿Qué consideraciones evaluaría a la hora de implementarlo?
+"""
+
+"""
+
+# ── ¿Qué consecuencias cree que tendría tener varias instancias de la DB?
+"""
+
+"""
+
+# ── ¿Qué pasa con las conexiones TCP?
+"""
+
+"""

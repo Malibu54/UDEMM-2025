706ba6e4710a7792b1f2f8abb3526c509bf6cd6d
~Oriana~ <deciimesool@icloud.com>
2026-04-12 21:56:08 -0300

    Update Ejercicio-07.py

--- Programacion-III/TP1P1/Ejercicio-07/Ejercicio-07.py
+++ Programacion-III/TP1P1/Ejercicio-07/Ejercicio-07.py
@@ -1,4 +1,20 @@
 from __future__ import annotations
 import abc
 import re
+import abc as abstractmethod
 import numpy as np
+
+# ─────────────────────────────────────────────
+# 1. NUEVA INTERFAZ (contrato del gateway)
+# ─────────────────────────────────────────────
+
+class PaymentGateway (abc.ABC):
+    @classmethod  
+    @abstractmethod  
+    def process (self.amount: float, currency_info: str ) -> bool:
+    ...
+            
+# ─────────────────────────────────────────────
+# 2. ADAPTER
+# ─────────────────────────────────────────────
+

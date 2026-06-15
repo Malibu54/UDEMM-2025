54d831a5c153b6880719a4543529d1601896bfb3
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:51:04 -0300

    Create nota_repository.py

---
+++ Programacion-III/TP-Integrador/app/repositories/nota_repository.py
@@ -0,0 +1,25 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class NotaRepository(ABC):
+
+    @abstractmethod
+    def guardar(self, nota):
+        pass
+
+    @abstractmethod
+    def actualizar(self, nota):
+        pass
+
+    @abstractmethod
+    def buscar_por_id(self, id):
+        pass
+
+    @abstractmethod
+    def obtener_todas(self):
+        pass
+
+    @abstractmethod
+    def eliminar(self, id):
+        pass

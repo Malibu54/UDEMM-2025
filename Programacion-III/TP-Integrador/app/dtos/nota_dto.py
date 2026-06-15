949c93a3f257d308c97e386d72cb9f4ad34c66d2
~Oriana~ <deciimesool@icloud.com>
2026-06-15 16:35:44 -0300

    Update nota_dto.py

--- Programacion-III/TP-Integrador/app/dtos/nota_dto.py
+++ Programacion-III/TP-Integrador/app/dtos/nota_dto.py
@@ -3,13 +3,19 @@ class NotaDTO:
     def __init__(
         self,
         titulo,
+        contenido,
         categoria,
+        visibilidad,
         fecha_publicacion,
+        hora_publicacion,
         autor
     ):
         self.titulo = titulo
+        self.contenido = contenido
         self.categoria = categoria
+        self.visibilidad = visibilidad
         self.fecha_publicacion = fecha_publicacion
+        self.hora_publicacion = hora_publicacion
         self.autor = autor
 
     @classmethod
@@ -17,7 +23,10 @@ class NotaDTO:
 
         return cls(
             titulo=nota.titulo,
+            contenido=nota.contenido,
             categoria=nota.categoria,
+            visibilidad=nota.visibilidad,
             fecha_publicacion=nota.fecha_publicacion,
+            hora_publicacion=nota.hora_publicacion,
             autor=nota.autor.nombre
         )

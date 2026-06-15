980a5fd7a470e5bb84ebbf27217d95728437ff0e
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:49:58 -0300

    Create test_filtros.py

---
+++ Programacion-III/TP-Integrador/app/tests/test_filtros.py
@@ -0,0 +1,19 @@
+import unittest
+
+from services.filtros.filtro_categoria import (
+    FiltroCategoria
+)
+
+
+class TestFiltros(unittest.TestCase):
+
+    def test_filtrar_categoria(self):
+
+        filtro = FiltroCategoria(
+            "EMERGENTE"
+        )
+
+        self.assertEqual(
+            filtro.categoria,
+            "EMERGENTE"
+        )

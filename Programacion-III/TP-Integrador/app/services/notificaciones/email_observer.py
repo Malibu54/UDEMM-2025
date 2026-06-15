a84687c3d7aeeee76919e7c40ac64d6a2b5b931b
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:50:23 -0300

    Create email_observer.py

---
+++ Programacion-III/TP-Integrador/app/services/notificaciones/email_observer.py
@@ -0,0 +1,13 @@
+from services.notificaciones.observer import (
+    Observer
+)
+
+
+class EmailObserver(Observer):
+
+    def actualizar(self, nota):
+
+        print(
+            f"[EMAIL] Nueva nota emergente: "
+            f"{nota.titulo}"
+        )

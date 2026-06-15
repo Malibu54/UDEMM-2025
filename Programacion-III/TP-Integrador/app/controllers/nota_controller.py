9e2a38a98d007a667abcee2f3d73637ba809fbe7
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:50:40 -0300

    Create nota_controller.py

---
+++ Programacion-III/TP-Integrador/app/controllers/nota_controller.py
@@ -0,0 +1,20 @@
+class NotaController:
+
+    def __init__(
+        self,
+        nota_service
+    ):
+        self.nota_service = nota_service
+
+    def crear_nota(
+        self,
+        nota
+    ):
+
+        nota = self.nota_service.crear_nota(
+            nota
+        )
+
+        return NotaDTO.from_nota(
+            nota
+        )

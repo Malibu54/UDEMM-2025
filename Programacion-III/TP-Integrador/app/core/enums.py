973a98c4926f6314774d8081d4b9e973346ecfa9
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:51:36 -0300

    Create enums.py

---
+++ Programacion-III/TP-Integrador/app/core/enums.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class CategoriaNota(Enum):
+
+    RELEVANTE = "RELEVANTE"
+
+    NEUTRAL = "NEUTRAL"
+
+    EMERGENTE = "EMERGENTE"
+
+    DEPRECATED = "DEPRECATED"
+
+
+class Visibilidad(Enum):
+
+    VISIBLE = "VISIBLE"
+
+    OCULTO = "OCULTO"

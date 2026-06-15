f1b7ad4d02c5a1bdb483a1d058a8f353fe5bc8f9
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:50:28 -0300

    Create filtro_categoria.py

---
+++ Programacion-III/TP-Integrador/app/services/filtros/filtro_categoria.py
@@ -0,0 +1,17 @@
+from services.filtros.filtro_strategy import (
+    FiltroStrategy
+)
+
+
+class FiltroCategoria(FiltroStrategy):
+
+    def __init__(self, categoria):
+        self.categoria = categoria
+
+    def aplicar(self, notas):
+
+        return [
+            nota
+            for nota in notas
+            if nota.categoria == self.categoria
+        ]

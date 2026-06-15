6358069577ccc0f704544865eea0327f280049b8
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:51:41 -0300

    Create database_factory.py

---
+++ Programacion-III/TP-Integrador/app/config/database_factory.py
@@ -0,0 +1,10 @@
+class DatabaseFactory:
+
+    @staticmethod
+    def create_repository(tipo):
+
+        if tipo == "sqlite":
+            return SQLiteNotaRepository()
+
+        if tipo == "oracle":
+            return OracleNotaRepository(...)

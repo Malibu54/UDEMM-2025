e49873590a36ba1551fc3aa2e07f6296d850e3e8
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:50:57 -0300

    Create sqlite_nota_repository.py

---
+++ Programacion-III/TP-Integrador/app/repositories/sqlite_nota_repository.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+from repositories.nota_repository import NotaRepository
+
+
+class SQLiteNotaRepository(NotaRepository):
+
+    def __init__(self):
+
+        self.conn = sqlite3.connect(
+            "betatrade.db",
+            check_same_thread=False
+        )
+
+    def guardar(self, nota):
+
+        cursor = self.conn.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO notas
+            (
+                titulo,
+                contenido,
+                categoria,
+                visibilidad
+            )
+            VALUES
+            (?, ?, ?, ?)
+            """,
+            (
+                nota.titulo,
+                nota.contenido,
+                nota.categoria,
+                nota.visibilidad
+            )
+        )
+
+        self.conn.commit()
+
+    def actualizar(self, nota):
+        pass
+
+    def buscar_por_id(self, id):
+        pass
+
+    def obtener_todas(self):
+        pass
+
+    def eliminar(self, id):
+        pass

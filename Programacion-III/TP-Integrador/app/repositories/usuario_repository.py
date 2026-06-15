e28dc68ad1188f298748098c2ae8adb628803a15
~Oriana~ <deciimesool@icloud.com>
2026-06-15 14:50:54 -0300

    Create usuario_repository.py

---
+++ Programacion-III/TP-Integrador/app/repositories/usuario_repository.py
@@ -0,0 +1,17 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class UsuarioRepository(ABC):
+
+    @abstractmethod
+    def guardar(self, usuario):
+        pass
+
+    @abstractmethod
+    def buscar_por_id(self, id):
+        pass
+
+    @abstractmethod
+    def obtener_todos(self):
+        pass
